9cdd9d9daa465987c2bb4e5334640bb5cc72ecad
AlchemR <102183958+AlchemR@users.noreply.github.com>
2022-09-02 00:50:17 -0700

    added reviews reset to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,8 @@ Cart.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!("carts")
 CartLedger.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!("cart_ledgers")
+Review.destroy_all
+ActiveRecord::Base.connection.reset_pk_sequence!("reviews")
 # do this for all tables and future tables
 
 u1 = User.create!({  username: "Sceptre", account_fname: "Sceptre", account_lname: "Electronics", email: "Sceptre@electronics.com", password: "123456" })
